399d3fccc47317df9adc4d824acf579b14c7b90e
helloCoding3 <joe_young1@163.com>
2023-06-24 09:58:12 +0800

    尝试7

--- functions/users/[[catchall]].js
+++ functions/users/[[catchall]].js
@@ -1,5 +1,6 @@
 export function onRequest(context) {
   // JSON.stringify(context.params.catchall)
-  const url = "https://cdn-images-1.medium.com/" + context.params.catchall.jion('/');
+  const path = JSON.parse(JSON.stringify(context.params.catchall));
+  const url = "https://cdn-images-1.medium.com/" + path.jion('/');
   return new Response(url);
 }
