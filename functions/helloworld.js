276492c9efc5d6e5a449107b95df8ad506afbb17
helloCoding3 <joe_young1@163.com>
2023-06-24 09:31:51 +0800

    尝试部署2

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,6 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
+export async function onRequest(context) {
+  const { request } = context;
+  console.log(`[LOGGING FROM /hello]: Request came from ${request.url}`);
+
+  return new Response(JSON.stringify(request));
 }
